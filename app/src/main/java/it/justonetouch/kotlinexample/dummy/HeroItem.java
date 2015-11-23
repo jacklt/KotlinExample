7ce8774385e832d7b87510baf3de0f5580f63186
Omar Miatello <omar.miatello@satispay.com>
2015-11-23 22:31:42 +0100

    Initial commit

---
+++ app/src/main/java/it/justonetouch/kotlinexample/dummy/HeroItem.java
@@ -0,0 +1,21 @@
+package it.justonetouch.kotlinexample.dummy;
+
+/**
+ * A dummy item representing a piece of content.
+ */
+public class HeroItem {
+    public final String id;
+    public final String content;
+    public final String details;
+
+    public HeroItem(String id, String content, String details) {
+        this.id = id;
+        this.content = content;
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+}
